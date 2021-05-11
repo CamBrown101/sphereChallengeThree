18219f948b8a02f5eedb8e1b2d19bf769014da35
cambrown101 <cameron-brown@live.com>
2021-05-11 11:22:22 -0700

    Create BottomSection

---
+++ components/BottomSection.tsx
@@ -0,0 +1,19 @@
+import React from "react";
+
+const BottomSection = () => {
+  return (
+    <div>
+      <h2>To get started:</h2>
+      <ul>
+        <li>Download our App for iOS here or Android here</li>
+        <li>Sign up and take our assessment</li>
+        <li>
+          Our algorithim will pair you with multiple guides to choose from
+        </li>
+        <li>Book your first session with a guide on us</li>
+        <li>If you enjoy it become a Member</li>
+      </ul>
+    </div>
+  );
+};
+export default BottomSection;
